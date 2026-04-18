454f8b671287d5cba096e581c99a6b3a6efd1bb0
Rehanbagwan007 <rehanbagwan10000@gmail.com>
2026-04-18 17:05:23 +0000

    add

--- webhook-backend/src/supabaseClient.ts
+++ webhook-backend/src/supabaseClient.ts
@@ -7,6 +7,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!supabaseUrl || !supabaseServiceKey) {
+  console.error('SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY must be defined in environment variables' , supabaseUrl, supabaseServiceKey);
   throw new Error('SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY must be defined');
 }
 
